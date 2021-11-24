53bd8eecc37e57f29c078a42d7ba5c94e0adf5d8
Antonio Melena <melenaantonio@gmail.com>
2021-11-24 00:25:07 -0600

    finished execept css

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,18 @@ const messages = [
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form");
+});
+
+router.post("/new", function (req, res, next) {
+  const messageText = req.body.messageText;
+  const messageUser = req.body.messageUser;
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect("/");
 });
 
 module.exports = router;
